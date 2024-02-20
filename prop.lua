b82867efbf3e2be7af02bf0530d0943dbbcf4128
Podaru Valentin Vlad <149790214+zeen29@users.noreply.github.com>
2024-02-20 14:20:02 +0200

    better and cleaner code thinking. credits Iulian R01

--- prop.lua
+++ prop.lua
@@ -1,127 +1,46 @@
-RegisterNetEvent('police:addentity:barrier');
-AddEventHandler('police:addentity:barrier', function()
-	local ped = PlayerPedId();
-	local hed = GetEntityHeading(ped);
-	local cod = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.5, 0.0);
-	local ecod = GetEntityCoords(ped)
+local allProps = {
+    ['barrier'] = {'Bariera Politie', 'F9', 'prop_barrier_work05'},
+    ['cone'] = {'Con Politie', 'F10', 'prop_mp_cone_01'},
+    ['tents'] = {'Cort  Politie', 'F11', 'prop_gazebo_02'},
+    ['desk'] = {'Masa Politie', 'F12', 'prop_table_03b_cs'},
 
-	RequestModel('prop_barrier_work05');
-	while not HasModelLoaded('prop_barrier_work05') do
-		Citizen.Wait(1);
-	end;
+    --['id'] = {'command name', 'command key', 'prop to place'},
+}
 
-	local closest = GetClosestObjectOfType(ecod.x, ecod.y, ecod.z, 1.2, 'prop_barrier_work05', true, true, true);
+RegisterNetEvent('police:addentity');
+AddEventHandler('police:addentity', function(what)
+    if not allProps[what] then return end
+    local prop = allProps[what][3]
+    
+    RequestModel(prop);
+    while not HasModelLoaded(prop) do
+        Citizen.Wait(500);
+    end;
 
-	if closest <= 0 then
-		object = CreateObject('prop_barrier_work05', cod.x, cod.y, cod.z, true, true);
-		local network = NetworkGetNetworkIdFromEntity(object);
-		SetNetworkIdCanMigrate(network, true);
-		SetEntityHeading(object, hed);
-		FreezeEntityPosition(object, true)
-		PlaceObjectOnGroundProperly(object);
-	else
-		DeleteEntity(object); DeleteEntity(closest);
-	end;
+    local playerPed = PlayerPedId();
+    local playerCoords = GetEntityCoords(playerPed)
+    local nearProp = GetClosestObjectOfType(playerCoords.x, playerCoords.y, playerCoords.z, 1.2, prop, true, true, true);
+
+    if nearProp and (nearProp > 0) then
+        DeleteEntity(nearProp);
+        return
+    end;
+
+    local wCoords = GetOffsetFromEntityInWorldCoords(playerPed, 0.0, 0.5, 0.0);
+    local playerHeading = GetEntityHeading(playerPed);
+
+    local object = CreateObject(prop, wCoords.x, wCoords.y, wCoords.z, true, true);
+    local network = NetworkGetNetworkIdFromEntity(object);
+    SetNetworkIdCanMigrate(network, true);
+    SetEntityHeading(object, playerHeading);
+    FreezeEntityPosition(object, true)
+    PlaceObjectOnGroundProperly(object);
 end)
 
-RegisterCommand('+barrier', function(source)
-	TriggerEvent('police:addentity:barrier', source)
-end)
-
-RegisterKeyMapping('+barrier', 'Bariera politie', 'keyboard', 'F9')
-
-RegisterNetEvent('police:addentity:cone');
-AddEventHandler('police:addentity:cone', function()
-	local ped = PlayerPedId();
-	local hed = GetEntityHeading(ped);
-	local cod = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.5, 0.0);
-	local ecod = GetEntityCoords(ped)
-
-	RequestModel('prop_mp_cone_01');
-	while not HasModelLoaded('prop_mp_cone_01') do
-		Citizen.Wait(1);
-	end;
-
-	local closest = GetClosestObjectOfType(ecod.x, ecod.y, ecod.z, 1.2, 'prop_mp_cone_01', true, true, true);
-
-	if closest <= 0 then
-		object = CreateObject('prop_mp_cone_01', cod.x, cod.y, cod.z, true, true);
-		local network = NetworkGetNetworkIdFromEntity(object);
-		SetNetworkIdCanMigrate(network, true);
-		SetEntityHeading(object, hed);
-		FreezeEntityPosition(object, true)
-		PlaceObjectOnGroundProperly(object);
-	else
-		DeleteEntity(object); DeleteEntity(closest);
-	end;
-end)
-
-RegisterCommand('+cone', function(source)
-	TriggerEvent('police:addentity:cone', source)
-end)
-
-RegisterKeyMapping('+cone', 'Con politie', 'keyboard', 'F10')
-
-RegisterNetEvent('police:addentity:tents');
-AddEventHandler('police:addentity:tents', function()
-	local ped = PlayerPedId();
-	local hed = GetEntityHeading(ped);
-	local cod = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.5, 0.0);
-	local ecod = GetEntityCoords(ped)
-
-	RequestModel('prop_gazebo_02');
-	while not HasModelLoaded('prop_gazebo_02') do
-		Citizen.Wait(1);
-	end;
-
-	local closest = GetClosestObjectOfType(ecod.x, ecod.y, ecod.z, 1.2, 'prop_gazebo_02', true, true, true);
-
-	if closest <= 0 then
-		object = CreateObject('prop_gazebo_02', cod.x, cod.y, cod.z, true, true);
-		local network = NetworkGetNetworkIdFromEntity(object);
-		SetNetworkIdCanMigrate(network, true);
-		SetEntityHeading(object, hed);
-		FreezeEntityPosition(object, true)
-		PlaceObjectOnGroundProperly(object);
-	else
-		DeleteEntity(object); DeleteEntity(closest);
-	end;
-end)
-
-RegisterCommand('+tents', function(source)
-	TriggerEvent('police:addentity:tents', source)
-end)
-
-RegisterKeyMapping('+tents', 'Cort politie', 'keyboard', 'F11')
-
-RegisterNetEvent('police:addentity:desk');
-AddEventHandler('police:addentity:desk', function()
-	local ped = PlayerPedId();
-	local hed = GetEntityHeading(ped);
-	local cod = GetOffsetFromEntityInWorldCoords(ped, 0.0, 0.5, 0.0);
-	local ecod = GetEntityCoords(ped)
-
-	RequestModel('prop_table_03b_cs');
-	while not HasModelLoaded('prop_table_03b_cs') do
-		Citizen.Wait(1);
-	end;
-
-	local closest = GetClosestObjectOfType(ecod.x, ecod.y, ecod.z, 1.2, 'prop_table_03b_cs', true, true, true);
-
-	if closest <= 0 then
-		object = CreateObject('prop_table_03b_cs', cod.x, cod.y, cod.z, true, true);
-		local network = NetworkGetNetworkIdFromEntity(object);
-		SetNetworkIdCanMigrate(network, true);
-		SetEntityHeading(object, hed);
-		FreezeEntityPosition(object, true)
-		PlaceObjectOnGroundProperly(object);
-	else
-		DeleteEntity(object); DeleteEntity(closest);
-	end;
-end)
-
-RegisterCommand('+desk', function(source)
-	TriggerEvent('police:addentity:desk', source)
-end)
-
-RegisterKeyMapping('+desk', 'Masa politie', 'keyboard', 'F12')
+for what, data in pairs(allProps) do
+    RegisterCommand('police_'..what, function()
+        TriggerEvent('police:addentity', what)
+    end)
+    
+    RegisterKeyMapping('police_'..what, data[1], 'keyboard', data[2])
+end
